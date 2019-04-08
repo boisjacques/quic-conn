17c55de43e7430afbc7e4a9ac60ba6d5144884fe
Manuel Kieweg <mail@manuelkieweg.de>
2019-04-08 18:08:53 +0100

    Updated

--- example/main.go
+++ example/main.go
@@ -1,17 +1,11 @@
 package main
 
 import (
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/tls"
-	"crypto/x509"
 	"encoding/binary"
-	"encoding/pem"
 	"flag"
 	"fmt"
 	"github.com/tylerwince/godbg"
 	"io"
-	"math/big"
 	"net"
 	"os"
 	"time"
@@ -23,21 +17,20 @@ const BUFFERSIZE = 1024
 
 func main() {
 	// utils.SetLogLevel(utils.LogLevelDebug)
+	var addr string
+	var file string
 
 	startServer := flag.Bool("s", false, "server")
 	startClient := flag.Bool("c", false, "client")
-	var addr string
+	flag.StringVar(&file, "file", "5MB.zip", "filename")
 	flag.StringVar(&addr, "addr", "", "address:port")
 	flag.Parse()
+
+	finishChan := make(chan bool)
 
 	if *startServer {
 		// start the server
 		go func() {
-			_, err := generateTLSConfig()
-			if err != nil {
-				panic(err)
-			}
-
 			ln, err := net.Listen("tcp", addr)
 			if err != nil {
 				panic(err)
@@ -50,33 +43,41 @@ func main() {
 			}
 			fmt.Println("Established connection")
 
-			go sendFileToClient(conn)
+			go sendFileToClient(conn, file, finishChan)
 		}()
 	}
 
 	if *startClient {
 		// run the client
 		go func() {
-			// tlsConf := &tls.Config{InsecureSkipVerify: true}
 			conn, err := net.Dial("tcp", addr)
 			if err != nil {
 				panic(err)
 			}
-			defer conn.Close()
 			fmt.Println("Connected to server, start receiving the file name and file size")
 			var fileSize int64
 			var fileNameLen int32
 
 			err = binary.Read(conn, binary.BigEndian, &fileSize)
-			godbg.Dbg(fileSize)
+			if err != nil {
+				panic(err)
+			}
 			err = binary.Read(conn, binary.BigEndian, &fileNameLen)
-			godbg.Dbg(fileNameLen)
+			if err != nil {
+				panic(err)
+			}
+
+			var handshake int32
+			handshake = 1
+			err = binary.Write(conn, binary.BigEndian, handshake)
+			if err != nil {
+				panic(err)
+			}
 
 			fileNameBuffer := make([]byte, fileNameLen)
-			_,err = io.ReadFull(conn, fileNameBuffer)
+			_, err = io.ReadFull(conn, fileNameBuffer)
 
 			fileName := string(fileNameBuffer[:fileNameLen])
-			godbg.Dbg(fileName)
 			newFile, err := os.Create("recvd_" + fileName)
 
 			if err != nil {
@@ -87,63 +88,47 @@ func main() {
 
 			for {
 				if (fileSize - receivedBytes) < BUFFERSIZE {
-					io.CopyN(newFile, conn, (fileSize - receivedBytes))
-					conn.Read(make([]byte, (receivedBytes+BUFFERSIZE)-fileSize))
+					_, err = io.CopyN(newFile, conn, (fileSize - receivedBytes))
+					if err != nil {
+						godbg.Dbg(err)
+					}
+					_, err = conn.Read(make([]byte, (receivedBytes+BUFFERSIZE)-fileSize))
+					if err != nil {
+						godbg.Dbg(err)
+					}
 					break
 				}
-				io.CopyN(newFile, conn, BUFFERSIZE)
+				_, err = io.CopyN(newFile, conn, BUFFERSIZE)
+				if err != nil {
+					godbg.Dbg(err)
+				}
 				receivedBytes += BUFFERSIZE
 			}
-			fmt.Println("Received file completely!")
+			if err == nil {
+				fmt.Println("Received file completely!")
+			} else {
+				godbg.Dbg(err)
+			}
+			err = conn.Close()
+			if err != nil {
+				godbg.Dbg(err)
+			}
+			finishChan <- true
 		}()
 	}
 
-	time.Sleep(time.Hour)
+	<-finishChan
 }
 
-func generateTLSConfig() (*tls.Config, error) {
-	key, err := rsa.GenerateKey(rand.Reader, 2048)
-	if err != nil {
-		return nil, err
-	}
-	template := x509.Certificate{
-		SerialNumber: big.NewInt(1),
-		NotBefore:    time.Now(),
-		NotAfter:     time.Now().Add(time.Hour),
-		KeyUsage:     x509.KeyUsageCertSign | x509.KeyUsageDigitalSignature,
-	}
-	certDER, err := x509.CreateCertificate(rand.Reader, &template, &template, &key.PublicKey, key)
-	if err != nil {
-		fmt.Println(err)
-		return nil, err
-	}
-	keyPEM := pem.EncodeToMemory(&pem.Block{
-		Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key),
-	})
-	b := pem.Block{Type: "CERTIFICATE", Bytes: certDER}
-	certPEM := pem.EncodeToMemory(&b)
-
-	tlsCert, err := tls.X509KeyPair(certPEM, keyPEM)
-	if err != nil {
-		return nil, err
-	}
-
-	return &tls.Config{
-		Certificates: []tls.Certificate{tlsCert},
-	}, nil
-}
-
-func sendFileToClient(connection net.Conn) {
+func sendFileToClient(connection net.Conn, f string, finishChan chan bool) {
 	fmt.Println("Connection Established!")
 	defer connection.Close()
-	file, err := os.Open("100MB.zip")
+	file, err := os.Open(f)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	fileInfo, err := file.Stat()
-	godbg.Dbg(fileInfo.Size())
-	godbg.Dbg(fileInfo.Name())
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -153,23 +138,29 @@ func sendFileToClient(connection net.Conn) {
 	fileSize = fileInfo.Size()
 	fileName := fileInfo.Name()
 	fileNameLen = int32(len(fileName))
-	godbg.Dbg(fileNameLen)
 	time.Sleep(10 * time.Millisecond)
 
 	err = binary.Write(connection, binary.BigEndian, fileSize)
 	if err != nil {
 		panic(err)
 	}
-	godbg.Dbg("sent filesize")
 	err = binary.Write(connection, binary.BigEndian, fileNameLen)
 	if err != nil {
 		panic(err)
 	}
-	godbg.Dbg("sent file name length")
-	_,err = io.WriteString(connection, fileName)
+	_, err = io.WriteString(connection, fileName)
 	if err != nil {
 		panic(err)
 	}
+	var handshake int32
+	err = binary.Read(connection, binary.BigEndian, &handshake)
+	if err != nil {
+		panic(err)
+	}
+	if handshake != 1 {
+		panic("handshake failed")
+	}
+
 	time.Sleep(10 * time.Millisecond)
 	sendBuffer := make([]byte, BUFFERSIZE)
 	fmt.Println("Start sending file!")
@@ -180,6 +171,8 @@ func sendFileToClient(connection net.Conn) {
 		}
 		connection.Write(sendBuffer)
 	}
-	fmt.Println("File has been sent, closing connection!")
-	return
+	fmt.Println("File has been sent, waiting 1 second!")
+	time.Sleep(1 * time.Second)
+	fmt.Println("Closing connection...")
+	finishChan <- true
 }
